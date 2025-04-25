968610788dbea9362d2f7455fe82773a1f49978b
Oneide Luiz Schneider <oneidels@gmail.com>
2025-04-25 18:30:22 +0200

    add comments for defaults

--- config/defaults.go
+++ config/defaults.go
@@ -2,6 +2,10 @@ package config
 
 // Global defaults for the CLI
 // CNI for Minikube: auto, bridge, calico, cilium, flannel, kindnet, or path to a CNI manifest (default: auto)
+// Driver for Kind: docker, containerd, or path to a driver binary (default: docker)
+// Driver for Minikube: docker, podman, virtualbox, vmware, kvm2, hyperkit, qemu, ssh, or path to a driver binary (default: docker)
+// - Minikube - container-runtime: docker, containerd, cri-o, or auto (default: auto)
+//   - Note: Not implemented yet
 const (
 	DefaultK8sVersion  = "1.32.0"
 	DefaultDriver      = "podman"
